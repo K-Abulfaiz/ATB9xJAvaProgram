24ab293398efe3c2d9d85d08a846075bca0412af
Abulfaiz <k.abulfaiz.ah@gmail.com>
2024-10-14 02:06:50 +0530

    8th oct practice

---
+++ src/Oct/ex_04102024/Lab004.java
@@ -0,0 +1,11 @@
+package Oct.ex_04102024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        int _Age=29;
+        System.out.printf(" My age is = %d", _Age);
+        _Age=30;
+        System.out.println("\n My age is = "+ (31));
+
+    }
+}
