f6a33f454f9482d0383ae8f40b552394e607b52c
Abulfaiz <k.abulfaiz.ah@gmail.com>
2024-10-14 01:33:36 +0530

    3rd oct assignment

--- src/Oct/ex_03102024/Lab002.java
+++ src/Oct/ex_03102024/Lab002.java
@@ -1,4 +1,19 @@
 package Oct.ex_03102024;
 
 public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(" 10x1=10\n 10x2=20\n 10x3=30\n 10x4=40\n 10x5=50\n 10x6=60\n 10x7=70\n 10x8=80\n 10x9=90\n 10x10=100");
+    }
+    /* Output will be as follow
+ 10*1=10
+ 10*2=20
+ 10*3=30
+ 10*4=40
+ 10*5=50
+ 10*6=60
+ 10*7=70
+ 10*8=80
+ 10*9=90
+ 10*10=100
+     */
 }
