a7ba3298b0fa57e4ecfd321af617d4666fb82541
Abulfaiz <k.abulfaiz.ah@gmail.com>
2024-10-14 01:43:32 +0530

    3rd oct assignment

---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,27 @@
+package Oct.ex_03102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("10x1= " +(10*1));
+        System.out.println("10x2= " +(10*2));
+        System.out.println("10x3= " +(10*3));
+        System.out.println("10x4= " +(10*4));
+        System.out.println("10x5= " +(10*5));
+        System.out.println("10x6= " +(10*6));
+        System.out.println("10x7= " +(10*7));
+        System.out.println("10x8= " +(10*8));
+        System.out.println("10x9= " +(10*9));
+        System.out.println("10x10="+(10*10));
+/*
+ 10*1=10
+ 10*2=20
+ 10*3=30
+ 10*4=40
+ 10*5=50
+ 10*6=60
+ 10*7=70
+ 10*8=80
+ 10*9=90
+ 10*10=100*/
+    }
+}
